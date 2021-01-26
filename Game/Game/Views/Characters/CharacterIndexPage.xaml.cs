89c7845b3f69dcaa7bb9795d09c56934b406970c
FangJennyYuan <yuanfang@seattleu.edu>
2021-01-26 13:00:38 -0800

    Added commnets to ReadItem_Clicked. Changed CharacterIndexPage.xaml.cs

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -41,8 +41,16 @@ namespace Game.Views.Characters
 			// await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
 		}
 
+		/// <summary>
+		/// Read a Character from list
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public async void ReadItem_Clicked(object sender, EventArgs e)
 		{
+			//TODO: logic to open Character page depending on character id
+
+			//TODO: used for now to link to Read.
 			await Navigation.PushModalAsync(new NavigationPage(new CharacterReadPage(viewModel)));
 		}
 	}
