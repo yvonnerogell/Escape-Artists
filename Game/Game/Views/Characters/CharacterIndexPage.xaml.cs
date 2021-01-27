e567448825b094f0ce187034cd9702c33410107e
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-01-27 13:19:11 -0800

    Renamed AddItem_Clicked to AddCharacterClicked. Changes made to CharacterIndexPage.xaml and CharacterIndexPage.xaml.cs.

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -37,7 +37,7 @@ namespace Game.Views.Characters
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		public async void AddItem_Clicked(object sender, EventArgs e)
+		public async void AddCharacter_Clicked(object sender, EventArgs e)
 		{
 			// await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
 		}
