9485d1cd8f5f3b817c89c3dac7b16eb39e995fcc
jonesm20 <jonesm20@seattleu.edu>
2021-01-30 22:51:36 -0500

    Added the CharacterCreatePage.xaml and the CharacterCreatePage.xaml.cs to the Characters folder within views

---
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Characters
+{
+    public partial class CharacterCreatePage : ContentPage
+    {
+        public CharacterCreatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
