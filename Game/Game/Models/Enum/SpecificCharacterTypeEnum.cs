70b3ffbf95a57dc57904d2d8473858cd602556f1
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-01 13:01:25 -0800

    Revert "Revert "Added CharacterTypeEnum.cs for character type.""
    Revert "Added CharacterTypeEnum.cs for character type."
    
    Added CharacterTypeEnum.cs for character type.

--- Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
+++ Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
@@ -1,15 +1,47 @@
-﻿namespace Game.Models
+﻿using System.Collections.Generic;
+namespace Game.Models
 {
     /// <summary>
     /// The player in the round, Monster or Character
     /// </summary>
-    public enum SpecificCharacterTypeEnum
+    public class SpecificCharacterTypeEnum
     {
-        // Not Known
-        Unknown = 0,
+        private SpecificCharacterTypeEnum(string value) { Value = value; }
 
-        // The Characters
-        Student = 10,
-        Parent = 11,
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Returns list of all characters
+        /// </summary>
+        /// <returns></returns>
+        public List<string> CharacterList()
+        {
+            var characterList = new List<string>()
+                {
+                "Smarty Pants",
+                "Overachiever",
+                "International Student",
+                "Prodigy",
+                "Second Career",
+                "Slacker",
+                "Procrastinator",
+                "Helicopter Parent",
+                "Cool Parent"
+                };
+            return characterList;
+        }
+
+        /// <summary>
+        /// returns individual character type
+        /// </summary>
+        public static SpecificCharacterTypeEnum SmartyPants { get { return new SpecificCharacterTypeEnum("Smarty Pants"); } }
+        public static SpecificCharacterTypeEnum Overachiever { get { return new SpecificCharacterTypeEnum("Overachiever"); } }
+        public static SpecificCharacterTypeEnum InternationalStudent { get { return new SpecificCharacterTypeEnum("International Student"); } }
+        public static SpecificCharacterTypeEnum Prodigy { get { return new SpecificCharacterTypeEnum("Prodigy"); } }
+        public static SpecificCharacterTypeEnum SecondCareer { get { return new SpecificCharacterTypeEnum("Second Career"); } }
+        public static SpecificCharacterTypeEnum Slacker { get { return new SpecificCharacterTypeEnum("Slacker"); } }
+        public static SpecificCharacterTypeEnum Procrastinator { get { return new SpecificCharacterTypeEnum("Procrastinator"); } }
+        public static SpecificCharacterTypeEnum HelicopterParent { get { return new SpecificCharacterTypeEnum("Helicopter Parent"); } }
+        public static SpecificCharacterTypeEnum CoolParent { get { return new SpecificCharacterTypeEnum("Cool Parent"); } }
     }
 }
