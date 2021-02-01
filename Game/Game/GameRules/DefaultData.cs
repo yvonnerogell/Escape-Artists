31b3f7381275b58eefcb783b047806a229a871b7
obakiner <onur.bakiner@gmail.com>
2021-01-31 16:42:45 -0800

    Changed Overachiever character's image to student.png on DefaultData.cs

--- Game/Game/GameRules/DefaultData.cs
+++ Game/Game/GameRules/DefaultData.cs
@@ -159,7 +159,7 @@ namespace Game.GameRules
                     SpecificPlayerType = SpecificPlayerTypeEnum.Student,
                     Level = 1,
                     MaxHealth = 100,
-                    ImageURI = "squid.jpg",
+                    ImageURI = "student.png",
                     Head = HeadString,
                     Necklace = NecklaceString,
                     PrimaryHand = PrimaryHandString,
