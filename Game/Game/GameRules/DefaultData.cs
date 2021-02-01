b0622a365b790952da34c065858025519e3bbc0b
obakiner <onur.bakiner@gmail.com>
2021-01-31 16:43:18 -0800

    Changed International Student character's image to student.png on DefaultData.cs

--- Game/Game/GameRules/DefaultData.cs
+++ Game/Game/GameRules/DefaultData.cs
@@ -175,7 +175,7 @@ namespace Game.GameRules
                     SpecificPlayerType = SpecificPlayerTypeEnum.Student,
                     Level = 1,
                     MaxHealth = 100,
-                    ImageURI = "squid.jpg",
+                    ImageURI = "student.png",
                     Head = HeadString,
                     Necklace = "None",
                     PrimaryHand = PrimaryHandString,
