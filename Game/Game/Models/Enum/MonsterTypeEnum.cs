79948571e0c067e43a8128e61d7aac30c92c6656
obakiner <onur.bakiner@gmail.com>
2021-02-03 13:17:43 -0800

    Created MonsterTypeEnum.cs in Models.Enum

---
+++ Game/Game/Models/Enum/MonsterTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Models
+{
+    /// <summary>
+    /// The player in the round for Monster
+    /// </summary>
+    public enum MonsterTypeEnum
+    {
+        // Not Known
+        Unknown = 0,
+
+        // The Characters
+        Faculty = 20,
+        Administrator = 21,
+    }
+}
