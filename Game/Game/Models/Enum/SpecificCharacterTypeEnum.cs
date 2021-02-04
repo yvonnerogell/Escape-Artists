d6c82d12d83aee3861c72b9420617586d6c60b58
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-04 11:14:13 -0800

    Partial Update: Changed SpecificCharacterTypeEnum to match other Enums.

--- Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
+++ Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
@@ -7,114 +7,28 @@ namespace Game.Models
     /// <summary>
     /// The player in the round for Character
     /// </summary>
-    public class SpecificCharacterTypeEnum
+    public enum SpecificCharacterTypeEnum
     {
-        private SpecificCharacterTypeEnum(string value) { Value = value; }
+        // Not specified
+        Unknown = 0,
 
-        public string Value { get; set; }
+        SmartyPants = 10,
 
-        // <summary>
-        /// Returns list of all characters
-        /// </summary>
-        /// <returns></returns>
-        public  List<string> CharacterList()
-        {
-            var characterList = new List<string>()
-                {
-                "Smarty Pants",
-                "Overachiever",
-                "International Student",
-                "Prodigy",
-                "Second Career",
-                "Slacker",
-                "Procrastinator",
-                "Helicopter Parent",
-                "Cool Parent"
-                };
-            return characterList;
-        }
+        Overachiever = 15,
 
-        /// <summary>
-        /// Returns list of all students
-        /// </summary>
-        /// <returns></returns>
-        public List<string> StudentList()
-        {
-            var characterList = new List<string>()
-                {
-                "Smarty Pants",
-                "Overachiever",
-                "International Student",
-                "Prodigy",
-                "Second Career",
-                "Slacker",
-                "Procrastinator"
-                };
-            return characterList;
-        }
+        InternationalStudent = 20,
 
-        /// <summary>
-        /// Returns list of all parents
-        /// </summary>
-        /// <returns></returns>
-        public List<string> ParentList()
-        {
-            var characterList = new List<string>()
-                {
-                "Helicopter Parent",
-                "Cool Parent"
-                };
-            return characterList;
-        }
+        Prodigy = 25,
 
-        /// <summary>
-        /// returns individual character type
-        /// </summary>
-        public static SpecificCharacterTypeEnum SmartyPants { get { return new SpecificCharacterTypeEnum("Smarty Pants"); } }
-        public static SpecificCharacterTypeEnum Overachiever { get { return new SpecificCharacterTypeEnum("Overachiever"); } }
-        public static SpecificCharacterTypeEnum InternationalStudent { get { return new SpecificCharacterTypeEnum("International Student"); } }
-        public static SpecificCharacterTypeEnum Prodigy { get { return new SpecificCharacterTypeEnum("Prodigy"); } }
-        public static SpecificCharacterTypeEnum SecondCareer { get { return new SpecificCharacterTypeEnum("Second Career"); } }
-        public static SpecificCharacterTypeEnum Slacker { get { return new SpecificCharacterTypeEnum("Slacker"); } }
-        public static SpecificCharacterTypeEnum Procrastinator { get { return new SpecificCharacterTypeEnum("Procrastinator"); } }
-        public static SpecificCharacterTypeEnum HelicopterParent { get { return new SpecificCharacterTypeEnum("Helicopter Parent"); } }
-        public static SpecificCharacterTypeEnum CoolParent { get { return new SpecificCharacterTypeEnum("Cool Parent"); } }
+        SecondCareer = 30,
 
+        Slacker = 35,
+
+        Procrastinator = 40,
+
+        HelicopterParent = 45,
+
+        CoolParent = 50
     }
-
-    /// <summary>
-    /// Helper for the Specific Character Type Enum Class
-    /// </summary>
-    public static class SpecificCharacterTypeEnumHelper
-    {
-        /// <summary>
-        /// Returns a list of strings of the enum for SpecificCharacterTypeEnum
-        /// </summary>
-        public static List<string> GetListItem
-        {
-            get
-            {
-                var myList = Enum.GetNames(typeof(SpecificCharacterTypeEnum)).ToList();
-                return myList;
-            }
-        }
-
-        /// <summary>
-        /// Returns a list of strings of the enum for SpecificCharacterTypeEnum
-        /// Removes the unknown
-        /// </summary>
-        public static List<string> GetListCharacter
-        {
-            get
-            {
-                var myList = Enum.GetNames(typeof(SpecificCharacterTypeEnum)).ToList().Where(m => m.ToString().Equals("Unknown") == false).ToList();
-                return myList;
-            }
-        }
-
-        
-    }
-
-
 
 }
