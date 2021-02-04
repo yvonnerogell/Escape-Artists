39101227fc67f50e70c59060561a103be190a2c1
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-04 11:22:54 -0800

    Added comments for each enum in SpecialCharacterTypeEnum.cs

--- Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
+++ Game/Game/Models/Enum/SpecificCharacterTypeEnum.cs
@@ -5,29 +5,38 @@ using System.Linq;
 namespace Game.Models
 {
     /// <summary>
-    /// The player in the round for Character
+    /// The specific Character enum type.  
     /// </summary>
     public enum SpecificCharacterTypeEnum
     {
         // Not specified
         Unknown = 0,
 
+        // Smarty pants is smart, has special ability and extra Head item location
         SmartyPants = 10,
 
+        // Overacheiver works hard and extra Head and Necklace item location
         Overachiever = 15,
 
+        // International Student is multi-cultured, has special ability and extra Head item location
         InternationalStudent = 20,
 
+        // Prodigy Student is smart, has special ability and extra Head item location
         Prodigy = 25,
 
+        // Second Career has real life experience, has special ability and Head and Necklace item location.
         SecondCareer = 30,
-
+        
+        // Slacker likes to chill and has extra Head and Necklace item location
         Slacker = 35,
 
+        // Procrastinator likes to wait, has special ability and extra Head item location
         Procrastinator = 40,
 
+        // Helicopter Parent likes to control and has special ability
         HelicopterParent = 45,
 
+        // Cool Parent likes to relax and has special ability
         CoolParent = 50
     }
 
