3ac9c47bd723eaac1b8b84a44963096183205a53
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-05 15:03:08 -0800

    Added new ItemTypeEnum.cs file to project.

---
+++ Game/Game/Models/Enum/ItemTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// The specific Item enum type.  
+    /// </summary>
+    public enum ItemTypeEnum
+    {
+        // Not specified
+        Unknown = 0
+    }
+
+}
