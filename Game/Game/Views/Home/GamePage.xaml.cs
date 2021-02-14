587c4bbb298e6fa1cc2cd4f0ce84ca92a90d19a2
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-13 18:07:03 -0800

    Changed village to School in GamePage.xaml and GamePage.xaml.cs.

--- Game/Game/Views/Home/GamePage.xaml.cs
+++ Game/Game/Views/Home/GamePage.xaml.cs
@@ -33,7 +33,7 @@ namespace Game.Views
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		public async void VillageButton_Clicked(object sender, EventArgs e)
+		public async void SchoolButton_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new VillagePage());
 		}
