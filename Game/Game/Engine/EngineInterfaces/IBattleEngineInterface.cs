9b54708d82491dc5167ad757a9d83497625eaa3b
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-17 11:31:23 -0800

    Added comments to IBattleEngineInterface.cs

--- Game/Game/Engine/EngineInterfaces/IBattleEngineInterface.cs
+++ Game/Game/Engine/EngineInterfaces/IBattleEngineInterface.cs
@@ -5,14 +5,30 @@ namespace Game.Engine.EngineInterfaces
 {
    public interface IBattleEngineInterface
     {
+        // The Round Variable
         IRoundEngineInterface Round { get; set; }
 
-        EngineSettingsModel EngineSettings { get;} 
+        // Engine Settings for this round
+        EngineSettingsModel EngineSettings { get;}
 
+        /// <summary>
+        /// Add the charcter to the character list
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         bool PopulateCharacterList(CharacterModel data);
 
+        /// <summary>
+        /// Start the Battle
+        /// </summary>
+        /// <param name="isAutoBattle"></param>
+        /// <returns></returns>
         bool StartBattle(bool isAutoBattle);
 
+        /// <summary>
+        /// End the Battle
+        /// </summary>
+        /// <returns></returns>
         bool EndBattle();
     }
 }
