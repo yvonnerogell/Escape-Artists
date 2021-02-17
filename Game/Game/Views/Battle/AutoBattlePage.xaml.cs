853776d2f9b6d262b2c2811ec16bf600a9a27f12
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-17 10:53:36 -0800

    Added comment to AutoBattlePage.xaml.cs.

--- Game/Game/Views/Battle/AutoBattlePage.xaml.cs
+++ Game/Game/Views/Battle/AutoBattlePage.xaml.cs
@@ -27,6 +27,11 @@ namespace Game.Views
 			InitializeComponent ();
 		}
 
+		/// <summary>
+		/// Defines what happens when the AutoBattleButton is clicked.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public async void AutobattleButton_Clicked(object sender, EventArgs e)
 		{
 			// Call into Auto Battle from here to do the Battle...
