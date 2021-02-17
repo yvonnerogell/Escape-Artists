f5018fecdeac0e2b77e0a9a59e9ec9bf735b1384
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-17 10:39:59 -0800

    Added comments to IDataStore.cs.

--- Game/Game/Services/IDataStore.cs
+++ Game/Game/Services/IDataStore.cs
@@ -9,13 +9,50 @@ namespace Game.Services
     /// <typeparam name="T"></typeparam>
     public interface IDataStore<T>
     {
+        /// <summary>
+        /// Create Async method for data store
+        /// </summary>
+        /// <param name="Data"></param>
+        /// <returns></returns>
         Task<bool> CreateAsync(T Data);
+
+        /// <summary>
+        /// Read Async method for datastore
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<T> ReadAsync(string id);
+
+        /// <summary>
+        /// Update Async method for datastore
+        /// </summary>
+        /// <param name="Data"></param>
+        /// <returns></returns>
         Task<bool> UpdateAsync(T Data);
+
+        /// <summary>
+        /// DeleteAsync method for datastore
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<bool> DeleteAsync(string id);
+
+        /// <summary>
+        /// Index Async method for datastore
+        /// </summary>
+        /// <returns></returns>
         Task<List<T>> IndexAsync();
 
+        /// <summary>
+        /// Wipes the data list asynchronously 
+        /// </summary>
+        /// <returns></returns>
         Task<bool> WipeDataListAsync();
+
+        /// <summary>
+        /// Indicates if datastore needs initialization, asynchronously
+        /// </summary>
+        /// <returns></returns>
         Task<bool> GetNeedsInitializationAsync();
     }
 }
