e81c5eb05cae6a79e1aac3f1082d5e768858f6c1
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-18 15:26:37 -0800

    Updated ScoreindexPageTests.cs.

--- UnitTests/Views/Score/ScoreIndexPageTests.cs
+++ UnitTests/Views/Score/ScoreIndexPageTests.cs
@@ -18,7 +18,7 @@ namespace UnitTests.Views
         ScoreIndexPage page;
 
         public ScoreIndexPageTests() : base(true) { }
-        
+
         [SetUp]
         public void Setup()
         {
