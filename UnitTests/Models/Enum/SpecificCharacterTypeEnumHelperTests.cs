b20626a921bb984c36ed9e1e1273839c63bb6441
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-19 15:03:52 -0800

    Added tests to SpecificCharacterTypeEnumHelperTests.cs

--- UnitTests/Models/Enum/SpecificCharacterTypeEnumHelperTests.cs
+++ UnitTests/Models/Enum/SpecificCharacterTypeEnumHelperTests.cs
@@ -6,5 +6,28 @@ namespace UnitTests.Models.Enum
 	[TestFixture]
 	class SpecificCharacterTypeEnumHelperTests
 	{
-	}
+        [Test]
+        public void SpecificCharacterTypeEnumHelperTests_GetListAll_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = SpecificCharacterTypeEnumHelper.GetListAll;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result.Count, 10);
+            Assert.Contains("Unknown", result);
+            Assert.Contains("SmartyPants", result);
+            Assert.Contains("Overachiever", result);
+            Assert.Contains("InternationalStudent", result);
+            Assert.Contains("Prodigy", result);
+            Assert.Contains("SecondCareer", result);
+            Assert.Contains("Slacker", result);
+            Assert.Contains("Procrastinator", result);
+            Assert.Contains("HelicopterParent", result);
+            Assert.Contains("CoolParent", result);
+        }
+    }
 }
