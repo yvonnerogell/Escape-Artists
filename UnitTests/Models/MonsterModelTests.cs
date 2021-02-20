7b4212d3bee73373794b41f583822500aca91321
obakiner <onur.bakiner@gmail.com>
2021-02-19 20:48:22 -0800

    Added MonsterModel_DropItemBasedOnCharacterType_GraduationOfficeAdministrator_Should_Pass to MonsterModelTests.cs

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -727,6 +727,23 @@ namespace UnitTests.Models
             Assert.AreEqual(null, result);
         }
 
+        [Test]
+        public void MonsterModel_DropItemBasedOnCharacterType_GraduationOfficeAdministrator_Should_Pass()
+        {
+            // Arrange
+            var data = new MonsterModel { SpecificMonsterTypeEnum = SpecificMonsterTypeEnum.GraduationOfficeAdministrator };
+
+            // Act
+
+            // create an ItemModel with Graduation OfficeAdmin SpecificMonsterType
+            var result = data.DropItemBasedOnCharacterType(data.SpecificMonsterTypeEnum);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(ItemTypeEnum.GraduationCapAndRobe, result.ItemType);
+        }
+
 
         /*
         [Test]
