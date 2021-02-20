f66d12970cd00c226e041d979ebb3fa8b7fee332
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-19 17:47:53 -0800

    Added test to BattleSettingsModelTests.cs for 100% coverage

--- UnitTests/Models/BattleSettingsModelTests.cs
+++ UnitTests/Models/BattleSettingsModelTests.cs
@@ -7,5 +7,24 @@ namespace UnitTests.Models
 	[TestFixture]
 	class BattleSettingsModelTests
 	{
-	}
+        [Test]
+        public void BattleSettingsModelTests_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BattleSettingsModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(false, result.AllowCriticalHit);
+            Assert.AreEqual(false, result.AllowCriticalMiss);
+            Assert.AreEqual(false, result.AllowMonsterItems);
+            Assert.AreEqual(BattleModeEnum.SimpleNext, result.BattleModeEnum);
+            Assert.AreEqual(HitStatusEnum.Default, result.CharacterHitEnum);
+            Assert.AreEqual(HitStatusEnum.Default, result.MonsterHitEnum);
+        }
+    }
 }
