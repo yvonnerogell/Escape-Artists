2e46f7f623be9740409dcb4dfd974daa0fde6fc9
jonesm20 <jonesm20@seattleu.edu>
2021-02-21 17:59:02 -0700

    Added test to cover non-null name for save_clicked method in ScoreUpdatePageTest

--- UnitTests/Views/Score/ScoreUpdatePageTests.cs
+++ UnitTests/Views/Score/ScoreUpdatePageTests.cs
@@ -101,6 +101,21 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void ScoreUpdatePage_Save_Clicked_Not_Null_Name_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = "Test";
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Test", page.ViewModel.Data.Name); // Got to here, so it happened...
+        }
+
+        [Test]
         public void ScoreUpdatePage_OnBackButtonPressed_Valid_Should_Pass()
         {
             // Arrange
