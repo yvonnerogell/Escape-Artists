b1730c06ba5d0d1a0f036b6262bdba4699df0ab3
jonesm20 <jonesm20@seattleu.edu>
2021-02-21 18:58:11 -0700

    Added test case for AutobattleButton_Clicked in GamePageTests

--- UnitTests/Views/Home/GamePageTests.cs
+++ UnitTests/Views/Home/GamePageTests.cs
@@ -81,5 +81,19 @@ namespace UnitTests.Views
             //Assert
             Assert.IsTrue(true); //Got to here, so it happened...
         }
+
+        [Test]
+        public void AutobattleButton_Clicked_Default_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+            page.AutobattleButton_Clicked(null, null);
+
+            //Reset
+
+            //Assert
+            Assert.IsTrue(true); //Got to here, so it happemed...
+        }
     }
 }
