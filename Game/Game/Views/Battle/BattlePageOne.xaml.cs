4f3c50b1c55fcfaaddb6b9f365b209ee7219c5e4
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-02-23 13:08:06 -0800

    Added battle messages to BattlePageOne.xaml and BAttlePageOne.xaml.cs.

--- Game/Game/Views/Battle/BattlePageOne.xaml.cs
+++ Game/Game/Views/Battle/BattlePageOne.xaml.cs
@@ -74,6 +74,13 @@ namespace Game.Views
             GPAValueLabel.Text = GetCharacterGPA(currentCharacter);
             HealthValueLabel.Text = GetCharacterHealth(currentCharacter);
 
+            var messages = GetBattleMessages();
+            BattleMessage1Label.Text = messages.ElementAt(0);
+            BattleMessage2Label.Text = messages.ElementAt(1);
+            BattleMessage3Label.Text = messages.ElementAt(2);
+            BattleMessage4Label.Text = messages.ElementAt(3);
+            BattleMessage5Label.Text = messages.ElementAt(4);
+
             // Set the Battle Mode
             // ShowBattleMode();
         }
@@ -155,6 +162,25 @@ namespace Game.Views
         {
             // TODO change to pull data from BattleEngineViewModel. 
             return currentCharacter.CurrentHealth.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// Returns the five most recent battle messages to display
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetBattleMessages()
+        {
+            // TODO change to pull data from BattleEngineViewModel. 
+            List<String> messages = new List<string>();
+            messages.Add("Faculty Honkey gives Minnie an exam.");
+            messages.Add("Goofey passes the exam with flying colors.");
+            messages.Add("Administrator Frothy gives Mike forms to fill out.");
+            messages.Add("Jocey fills out all the forms from Frothy.");
+            messages.Add("Minnie studies hard from Professor Frank's exam.");
+
+            return messages;
+
         }
 
         /*
