b53d8782c6734da3f097f8773e783138e346dd9e
obakiner <onur.bakiner@gmail.com>
2021-02-23 19:46:53 -0800

    Deleted made-up data from BattlePageTwo.xaml.cs

--- Game/Game/Views/Battle/BattlePageTwo.xaml.cs
+++ Game/Game/Views/Battle/BattlePageTwo.xaml.cs
@@ -63,12 +63,8 @@ namespace Game.Views
             // Ask the Game engine to select who goes first
             BattleEngineViewModel.Instance.Engine.Round.SetCurrentAttacker(null);
 
-            // Identifying elements of the BattlePage
-            var currentCharacters = new List<CharacterModel>();
-            var currentMonsters = new List<MonsterModel>();
             
             currentMonsters.Add(new MonsterModel { Name = "Honkey", MonsterTypeEnum = MonsterTypeEnum.Faculty, SpecificMonsterTypeEnum = SpecificMonsterTypeEnum.AdjunctFaculty, PlayerType = PlayerTypeEnum.Monster });
-            currentCharacters.Add(new CharacterModel { Name = "Minnie", CurrentHealth = 15, GPA = 67, CharacterTypeEnum = CharacterTypeEnum.Student, PlayerType = PlayerTypeEnum.Character});
             
             var currentAttacker = new PlayerInfoModel(currentMonsters[0]);
             var currentDefender = new PlayerInfoModel(currentCharacters[0]);
