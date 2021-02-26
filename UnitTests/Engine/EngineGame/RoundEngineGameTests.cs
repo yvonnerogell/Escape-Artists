d74a86ae488d5513cd5927b5f607397519b6f558
FangJennyYuan <yuanfang@seattleu.edu>
2021-02-26 14:37:51 -0800

    Added tests for AddMonstersToRound in RoundEngineGameTests.cs

--- UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
@@ -25,7 +25,7 @@ namespace UnitTests.Engine.EngineGame
             Engine.Round.ClearLists();
 
             //Start the Engine in AutoBattle Mode
-            //Engine.StartBattle(true);   
+            Engine.StartBattle(true);   
         }
 
         [TearDown]
@@ -49,5 +49,66 @@ namespace UnitTests.Engine.EngineGame
             Assert.IsNotNull(result);
         }
         #endregion Constructor
+
+        #region AddMonstersToRound
+        
+        [Test]
+        public void RoundEngine_AddMonstersToRound_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            
+            var result = Engine.Round.AddMonstersToRound();
+
+
+            // Reset
+            MonsterIndexViewModel.Instance.ForceDataRefresh();
+            //Engine.EngineSettings.MonsterList = null;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        
+        
+        [Test]
+        public void RoundEngine_AddMonstersToRound_Valid_HighLevelCharacter_Should_Pass()
+        {
+            // Arrange
+            
+            var ContainHighLevelCharacter = (Engine.EngineSettings.CharacterList.Find(m => (m.Level > 17)) != null);
+            if (!ContainHighLevelCharacter)
+            {
+                Engine.EngineSettings.CharacterList.FirstOrDefault().Level = 18;
+            }
+            
+            // Act
+            var data = new CharacterModel { Level = 1, MaxHealth = 10 };
+
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+
+            data = new CharacterModel { Level = 18, MaxHealth = 10 };
+            Engine.EngineSettings.CharacterList.Add(new PlayerInfoModel(data));
+
+            Engine.Round.AddMonstersToRound();
+
+            var result = Engine.EngineSettings.MonsterList.Find(m => m.SpecificMonsterTypeEnum == SpecificMonsterTypeEnum.GraduationOfficeAdministrator);
+
+            // Reset
+            MonsterIndexViewModel.Instance.ForceDataRefresh();
+            CharacterIndexViewModel.Instance.ForceDataRefresh();
+            //Engine.EngineSettings.MonsterList = null;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        
+        
+        #endregion AddMonstersToRound
+
     }
 }
