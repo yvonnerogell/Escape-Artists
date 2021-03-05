e23462e60df5d094be89ab13b89fde2a0b87329f
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-03-05 11:11:33 -0800

    Added tests for PickupItemsFromPool() to RoundEngineGameTests.cs.

--- UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
+++ UnitTests/Engine/EngineGame/RoundEngineGameTests.cs
@@ -156,6 +156,58 @@ namespace UnitTests.Engine.EngineGame
         }
 
         [Test]
+        public void RoundEngine_PickupItemsFromPool_Auto_Valid_Student_Level_18_Should_Pass()
+        {
+            // Arrange
+            PlayerInfoModel character = new PlayerInfoModel(new CharacterModel { Level = 18, CharacterTypeEnum = CharacterTypeEnum.Student});
+            var save = Engine.EngineSettings.ItemPool;
+            Engine.EngineSettings.ItemPool.Clear();
+            Engine.EngineSettings.ItemPool.Add(new ItemModel { ItemType = ItemTypeEnum.GraduationCapAndRobe });
+
+            // Act
+            bool result = Engine.Round.PickupItemsFromPool(character);
+
+            // Reset
+            Engine.EngineSettings.ItemPool.Clear();
+            Engine.EngineSettings.ItemPool = save;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void RoundEngine_PickupItemsFromPool_Auto_Valid_Student_Level_15_Should_Pass()
+        {
+            // Arrange
+            PlayerInfoModel character = new PlayerInfoModel(new CharacterModel { Level = 15, CharacterTypeEnum = CharacterTypeEnum.Student });
+
+            // Act
+
+            bool result = Engine.Round.PickupItemsFromPool(character);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void RoundEngine_PickupItemsFromPool_Auto_Valid_Parent_Level_15_Should_Pass()
+        {
+            // Arrange
+            PlayerInfoModel character = new PlayerInfoModel(new CharacterModel { Level = 15, CharacterTypeEnum = CharacterTypeEnum.Parent });
+
+            // Act
+
+            bool result = Engine.Round.PickupItemsFromPool(character);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
         public void RoundEngine_PickupItemsFromPool_Manual_Valid_Default_Should_Pass()
         {
             // Arrange
