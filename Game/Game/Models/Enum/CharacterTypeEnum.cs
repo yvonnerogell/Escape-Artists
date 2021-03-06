ac402056c680ec93a2cdc577f97e00c31723c71f
FangJennyYuan <yuanfang@seattleu.edu>
2021-03-05 19:02:15 -0800

    Added helper method to assist with battle messages in CharacterTypeEnum.cs

--- Game/Game/Models/Enum/CharacterTypeEnum.cs
+++ Game/Game/Models/Enum/CharacterTypeEnum.cs
@@ -14,4 +14,50 @@
         // Character is of parent type
         Parent = 11,
     }
+
+    /// <summary>
+    /// Helper for the Monster or Character
+    /// </summary>
+    public static class CharacterTypeEnumHelper
+    {
+        /// <summary>
+        /// Helper used get msg
+        /// </summary>
+        public static string getAttackMessage(CharacterTypeEnum attacker, MonsterTypeEnum target)
+        {
+            string msg = "";
+            switch (attacker)
+            {
+                case CharacterTypeEnum.Student:
+                    switch (target)
+                    {
+                        case MonsterTypeEnum.Faculty:
+                            msg = " pass an exam from ";
+                            break;
+
+                        case MonsterTypeEnum.Administrator:
+                            msg = " finished the paper work from ";
+                            break;
+                    }
+                    break;
+
+                case CharacterTypeEnum.Parent:
+                    switch (target)
+                    {
+                        case MonsterTypeEnum.Faculty:
+                            msg = " puts pressure to make exam easier on ";
+                            break;
+
+                        case MonsterTypeEnum.Administrator:
+                            msg = " complain about time needed to process paperwork from ";
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return msg;
+        }
+    }
 }
