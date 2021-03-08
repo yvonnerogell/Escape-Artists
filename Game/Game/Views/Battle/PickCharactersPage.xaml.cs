a47cc6e6211015515935a8819c55a7fb534a2c73
meljones <jonesm20@seattleu.edu>
2021-03-07 21:09:13 -0700

    Changed background color of charactor image button when selected vs. not selected

--- Game/Game/Views/Battle/PickCharactersPage.xaml.cs
+++ Game/Game/Views/Battle/PickCharactersPage.xaml.cs
@@ -15,14 +15,6 @@ namespace Game.Views
 {
     /// <summary>
     /// Selecting Characters for the Game
-    /// 
-    /// TODO: Team
-    /// Mike's game allows duplicate characters in a party (6 Mikes can all fight)
-    /// If you do not allow duplicates, change the code below
-    /// Instead of using the database list directly make a copy of it in the viewmodel
-    /// Then have on select of the database remove the character from that list and add to the part list
-    /// Have select from the party list remove it from the party list and add it to the database list
-    ///
     /// </summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
     [DesignTimeVisible(false)]
@@ -217,12 +209,14 @@ namespace Game.Views
             {
                 BattleEngineViewModel.Instance.PartyCharacterList.Remove(data);
                 is_added = false;
+                button.BackgroundColor = Color.Transparent;
             }
             else if (BattleEngineViewModel.Instance.PartyCharacterList.Count() < BattleEngineViewModel.Instance.Engine.EngineSettings.MaxNumberPartyCharacters)
             {
                 //Add character to the instance partcharacterlist
                 BattleEngineViewModel.Instance.PartyCharacterList.Add(data);
                 is_added = true;
+                button.BackgroundColor = Color.Green;
             }
             UpdateNextButtonState();
         }
@@ -232,23 +226,25 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="args"></param>
-        public void OnPartyCharacterItemSelected(object sender, SelectedItemChangedEventArgs args)
+        public void OnPartySelectionChanged(object sender, SelectionChangedEventArgs args)
         {
-            CharacterModel data = args.SelectedItem as CharacterModel;
-            if (data == null)
+            var button = sender as ImageButton;
+            String characterId = button.CommandParameter as String;
+            CharacterModel data = ViewModel.Dataset.FirstOrDefault(itm => itm.Id == characterId);
+            if (is_added == false)
+            {
+                BattleEngineViewModel.Instance.PartyCharacterList.Add(data);
+                is_added = true;
+            } else if (is_added == true)
+            {
+                BattleEngineViewModel.Instance.PartyCharacterList.Remove(data);
+                is_added = false;
+            } else
             {
                 return;
             }
 
-            
-
-            // Manually deselect Character.
-            //PartyListView.SelectedItem = null;
-
-            // Remove the character from the list
-            //BattleEngineViewModel.Instance.PartyCharacterList.Remove(data);
-
-            //     UpdateNextButtonState();
+            UpdateNextButtonState();
         }
 
         /// <summary>
