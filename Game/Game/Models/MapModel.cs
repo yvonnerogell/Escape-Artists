1e6737842f7563eb372e98e56823f89177f8e94f
FangJennyYuan <yuanfang@seattleu.edu>
2021-03-10 13:10:00 -0800

    Added new method for speed calcuation for scenario 35 in MapModel.cs

--- Game/Game/Models/MapModel.cs
+++ Game/Game/Models/MapModel.cs
@@ -313,6 +313,31 @@ namespace Game.Models
             return Result;
         }
 
+        /// Walk the Map and Find the Location that is close to the target based on attacker's speed.
+        /// </summary>
+        /// <param name="Target"></param>
+        /// <returns></returns>
+        public MapModelLocation ReturnClosestEmptyLocationBasedOnSpeed(MapModelLocation Attacker, MapModelLocation Target)
+        {
+            MapModelLocation Result = null;
+
+            int LowestDistance = int.MaxValue;
+            int MaxTravelForAttacker = Attacker.Player.Speed;
+
+            foreach (var data in GetEmptyLocations())
+            {
+                var distanceToTarget = CalculateDistance(data, Target);
+                var distanceFromAttacker = CalculateDistance(data, Attacker);
+                if (distanceToTarget < LowestDistance && distanceFromAttacker <= MaxTravelForAttacker)
+                {
+                    Result = data;
+                    LowestDistance = distanceToTarget;
+                }
+            }
+
+            return Result;
+        }
+
         /// <summary>
         /// See if the Attacker is next to the Defender by the distance of Range
         /// 
