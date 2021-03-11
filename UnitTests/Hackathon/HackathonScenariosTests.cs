648fce700dd5e27a2ebb4b74340a6a00524f75e3
obakiner <onur.bakiner@gmail.com>
2021-03-10 18:29:13 -0800

    Updated HackathonScenarioTests for Scenario 25 to include Feet

--- UnitTests/Hackathon/HackathonScenariosTests.cs
+++ UnitTests/Hackathon/HackathonScenariosTests.cs
@@ -573,6 +573,93 @@ namespace Scenario
         }
 
         [Test]
+        public async Task HackathonScenario_Scenario_25_Valid_Feet_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      25
+            *      
+            * Description: 
+            * Drop an Item if the Target took a hit (probabilistic)
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      TurnEngineBase.cs – Added LoseDamagedItem() method into AddDamage() in TurnBaseEngine.cs 
+            *      TurnEngineBase.cs – Added LoseDamagedItem_Probability as a hardcoded public int
+            *      PlayerInfoModel.cs - Added a LoseDamagedItem switch
+            *      
+            * Test Algorithm:
+            *      Create Character with a droppable item in Feet
+            *      Create a Monster Hit
+            *      Set CurrentAction to ActionEnum.Attack
+            *      Set hackathon debug to true
+            *  
+            *      Startup Battle
+            *      Run Auto Battle
+            * 
+            * Test Conditions:
+            *      
+            * 
+            * Validation:
+            *      Verify Battle Returned True
+            *      Verify that DroppedItemList has a new addition
+            *  
+            */
+
+            //Arrange
+
+            // This works in Attack mode
+            EngineViewModel.EngineGame.EngineSettings.HackathonDebug = true;
+
+            EngineViewModel.EngineGame.EngineSettings.CurrentAction = ActionEnum.Attack;
+
+            // Set Character Conditions
+            EngineViewModel.EngineGame.EngineSettings.MaxNumberPartyCharacters = 1;
+            var character = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = 5,
+                                Level = 5,
+                                Feet = "Skateboard",
+                                CurrentHealth = 50,
+                                ExperienceTotal = 10,
+                                ExperienceRemaining = 10,
+                                Name = "WeakDefender"
+                            });
+            character.LoseDamagedItem = true;
+            EngineViewModel.EngineGame.EngineSettings.CharacterList.Clear();
+            EngineViewModel.EngineGame.EngineSettings.CharacterList.Add(character);
+            // Setting a monster as attacker, and our character as defender
+            EngineViewModel.EngineGame.EngineSettings.CurrentAttacker = EngineViewModel.EngineGame.EngineSettings.MonsterList[0];
+            //EngineViewModel.EngineGame.EngineSettings.CurrentDefender = character;
+
+            // Monster hits, and to make things simpler, the character misses
+            EngineViewModel.EngineGame.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Hit;
+            EngineViewModel.EngineGame.EngineSettings.BattleSettingsModel.CharacterHitEnum = HitStatusEnum.Miss;
+            EngineViewModel.EngineGame.EngineSettings.BattleMessagesModel.HitStatus = HitStatusEnum.Hit;
+
+            // Start with empty Dropped Item List
+            //EngineViewModel.EngineGame.EngineSettings.BattleScore.ItemModelDropList.Clear();
+
+            //Act
+            var result = await EngineViewModel.AutoBattleEngineGame.RunAutoBattle();
+
+            //Assert
+            Assert.IsTrue(result);
+            Assert.IsNull(EngineViewModel.EngineGame.EngineSettings.CurrentDefender.Feet);
+
+            // Reset
+            EngineViewModel.EngineGame.EngineSettings.CurrentAttacker = null;
+            EngineViewModel.EngineGame.EngineSettings.CurrentDefender = null;
+            EngineViewModel.EngineGame.EngineSettings.BattleSettingsModel.MonsterHitEnum = HitStatusEnum.Default;
+            EngineViewModel.EngineGame.EngineSettings.BattleSettingsModel.CharacterHitEnum = HitStatusEnum.Default;
+            EngineViewModel.EngineGame.EngineSettings.CharacterList.Clear();
+            EngineViewModel.EngineGame.EngineSettings.HackathonDebug = false;
+            EngineViewModel.EngineGame.EngineSettings.BattleMessagesModel.HitStatus = HitStatusEnum.Unknown;
+        }
+
+
+
+        [Test]
         public async Task HackathonScenario_Scenario_25_Valid_Necklace_Should_Pass()
         {
             /* 
