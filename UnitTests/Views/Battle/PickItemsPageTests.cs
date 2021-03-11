93d5f5e1de0ed0962fde56a6b7d240e80457d5b8
FangJennyYuan <yuanfang@seattleu.edu>
2021-03-10 17:59:23 -0800

    Changed call from Mike's Game to EngineGame in PickItemsPageTests.cs

--- UnitTests/Views/Battle/PickItemsPageTests.cs
+++ UnitTests/Views/Battle/PickItemsPageTests.cs
@@ -31,7 +31,8 @@ namespace UnitTests.Views
             Application.Current = app;
 
             // For now, set the engine to the Koenig Engine, change when ready 
-            BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
+            //BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
+            BattleEngineViewModel.Instance.SetBattleEngineToGame();
 
             page = new PickItemsPage();
         }
