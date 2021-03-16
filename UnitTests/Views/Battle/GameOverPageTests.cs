8bea709174bf658581ef7e211a5b24f9bcfcd40e
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-03-16 13:00:19 -0700

    Removed commented out code from GameOverPageTests.cs.

--- UnitTests/Views/Battle/GameOverPageTests.cs
+++ UnitTests/Views/Battle/GameOverPageTests.cs
@@ -31,8 +31,6 @@ namespace UnitTests.Views
             app = new App();
             Application.Current = app;
 
-            // For now, set the engine to the Koenig Engine, change when ready 
-            //BattleEngineViewModel.Instance.SetBattleEngineToKoenig();
             BattleEngineViewModel.Instance.SetBattleEngineToGame();
 
             page = new GameOverPage();
