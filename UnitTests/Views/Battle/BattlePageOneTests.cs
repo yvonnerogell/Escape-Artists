ab86bfe31b2101701190e5f4dcde36cc4c75aeae
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-03-15 20:20:20 -0700

    Added test for RoundOverButton navigation stack to BattlePageOneTests.cs.

--- UnitTests/Views/Battle/BattlePageOneTests.cs
+++ UnitTests/Views/Battle/BattlePageOneTests.cs
@@ -416,6 +416,26 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void BattlePage_RoundOverButton_Clicked_NavigationStack_Should_Pass()
+        {
+            // Arrange
+            BattlePageOne page1 = new BattlePageOne();
+
+            page1.Navigation.PushAsync(new HomePage());
+            page1.Navigation.PushAsync(new GamePage());
+            page1.Navigation.PushAsync(new PickCharactersPage());
+            page1.Navigation.PushAsync(new NewRoundPage());
+
+            // Act
+            page1.RoundOverButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
         /*
 
         [Test]
@@ -433,7 +453,7 @@ namespace UnitTests.Views
         }
         */
 
-        
+
         [Test]
         public void BattlePage_ExitButton_Clicked_Default_Should_Pass()
         {
