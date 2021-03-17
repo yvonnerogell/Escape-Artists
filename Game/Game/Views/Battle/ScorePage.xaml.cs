9fe9b8a93cdbaa7a1db39f1120ac783f5617216e
Yvonne Rogell <yvonne.rogell@gmail.com>
2021-03-16 17:18:30 -0700

    removed await from ScorePage.xaml.cs.

--- Game/Game/Views/Battle/ScorePage.xaml.cs
+++ Game/Game/Views/Battle/ScorePage.xaml.cs
@@ -206,7 +206,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void CloseButton_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PopAsync();
+            Navigation.PopAsync();
         }
     }
 }
